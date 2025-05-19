2d9c3cf48c25fa5cb4c0047dbf2eea1d5130b4c0
alanrichter53@gmail.com <alanrichter53@gmail.com>
2025-05-19 16:00:18 +0200

    Added IsValid function that checks during choosing an ingredient whether user input come under the limit or not, if not, program outputs that there is no such products and let's you choose again, until an user choose a valid ingredient.

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿SandwichShop shop = new SandwichShop();
 Client client = new Client();
-Sandwich sandwich = shop.OrderSandwich(client);
+Sandwich sandwich = shop.OrderSandwich(client, shop);
 Console.WriteLine(sandwich);
 Console.WriteLine($"Your balance: {client.Balance}");
 
@@ -11,7 +11,7 @@ class SandwichShop
     private List<decimal> spreadPrizes = new List<decimal> { 1.00m, 3.00m, 6.00m, 8.00m, 3.50m, 1.00m, 5.00m, 0.00m};
     private List<decimal> meatPrizes = new List<decimal> { 1.00m, 8.00m, 6.00m, 5.50m, 5.50m, 7.50m, 9.00m, 4.00m, 6.50m, 6.50m, 3.00m, 0.00m};
     private List<decimal> cheesePrizes = new List<decimal> { 7.00m, 4.00m, 2.50m, 8.00m, 10.00m, 5.30m, 6.00m, 6.10m, 4.00m, 10.10m, 2.50m, 0.00m};
-    private List<decimal> vegetablePrizes = new List<decimal> { 2.20m, 3.00m, 0.60m, 4.00m, 0.70m, 1.50m, 1.80m, 0.50m, 0.30m, 0.50m, 4.00m, 1.00m, 1.20m, 0.00m};
+    private List<decimal> vegetablePrizes = new List<decimal> { 2.20m, 3.00m, 0.60m, 4.00m, 0.70m, 1.50m, 1.80m, 0.50m, 0.30m, 0.50m, 4.00m, 1.00m, 1.20m, 1.60m, 0.00m};
     private List<decimal> saucePrizes = new List<decimal> { 2.50m, 2.00m, 4.00m, 5.00m, 5.00m, 5.50m, 6.00m, 7.00m, 5.50m, 5.00m, 7.50m, 3.50m, 6.00m};
     public SandwichShop()
     {
@@ -23,7 +23,7 @@ class SandwichShop
         Console.WriteLine("----------BUNS----------");
         for (int i = 1; i < Enum.GetNames(typeof(Bun)).Length; i++)
         {
-            Console.WriteLine($"{i}. {(Bun)i} {bunPrizes[i-1]}zł");
+            Console.WriteLine($"{i}. {(Bun)i} {bunPrizes[i - 1]}zł");
         }
 
         Console.WriteLine("\n----------SPREADS----------");
@@ -57,30 +57,91 @@ class SandwichShop
         }
     }
 
-    public Sandwich OrderSandwich(Client client)
+    private bool IsValid(int choice, Type enumType)
     {
+        if (choice >= 1 && choice <= Enum.GetValues(enumType).Length - 1)
+        {
+            return true;
+        }
+
+        Console.WriteLine("We don't have that product.");
+        return false;
+    }
+
+    public Sandwich OrderSandwich(Client client, SandwichShop shop)
+    {
+
         int bun, spread, meat, cheese, vegetable, sauce;
         Console.WriteLine("Hi, what can I get for you?");
         GetListOfIngredients();
 
-        Console.WriteLine("What type of bun would you like?");
-        bun = int.Parse(Console.ReadLine());
-        Console.WriteLine("Nice, now pick your favourite spread, please.");
-        spread = int.Parse(Console.ReadLine());
-        Console.WriteLine("Good choice, time for a meat :).");
-        meat = int.Parse(Console.ReadLine());
-        Console.WriteLine("Cheese is a must have.");
-        cheese = int.Parse(Console.ReadLine());
-        Console.WriteLine("Some veggies?");
-        vegetable = int.Parse(Console.ReadLine());
-        Console.WriteLine("And a sauce for a finish");
-        sauce = int.Parse(Console.ReadLine());
+        while (true)
+        {
+            Console.WriteLine("What type of bun would you like?");
+            bun = int.Parse(Console.ReadLine()!);
+            //if(int.TryParse(input, out int liczba))
+            //{
+            //    bun = liczba;
+            //    IsValid(bun);
+            //}
 
-        client.Balance = client.Balance - bunPrizes[bun - 1]
-            - spreadPrizes[spread - 1] - meatPrizes[meat - 1] - cheesePrizes[cheese - 1] - vegetablePrizes[vegetable - 1] - saucePrizes[sauce - 1];
+            if (IsValid(bun, typeof(Bun)))
+                 break;
+        }
 
-    Console.WriteLine($"That's the chad sandwich that you have composed there! It'il cost: {bunPrizes[bun-1]
-            + spreadPrizes[spread-1] + meatPrizes[meat-1] + cheesePrizes[cheese-1] + vegetablePrizes[vegetable-1] + saucePrizes[sauce-1]}");
+        while (true)
+        {
+            Console.WriteLine("Nice, now pick your favourite spread, please.");
+            spread = int.Parse(Console.ReadLine()!);
+
+            if (IsValid(spread, typeof(Spread)))
+                break;
+        }
+
+        while (true)
+        {
+            Console.WriteLine("Good choice, time for a meat :).");
+            meat = int.Parse(Console.ReadLine()!);
+
+            if (IsValid(meat, typeof(Meat)))
+                break;
+        }
+
+        while (true)
+        {
+            Console.WriteLine("Cheese is a must have.");
+            cheese = int.Parse(Console.ReadLine()!);
+
+            if (IsValid(cheese, typeof(Cheese)))
+                break;
+        }
+
+        while (true)
+        {
+
+            Console.WriteLine("Some veggies?");
+            vegetable = int.Parse(Console.ReadLine()!);
+
+            if (IsValid(vegetable, typeof(Vegetable)))
+                break;
+        }
+
+        while (true)
+        {
+            Console.WriteLine("And a sauce for a finish");
+            sauce = int.Parse(Console.ReadLine()!);
+
+
+            if (IsValid(sauce, typeof(Sauce)))
+                break;
+        }
+
+        decimal total = bunPrizes[bun - 1]
+            + spreadPrizes[spread - 1] + meatPrizes[meat - 1] + cheesePrizes[cheese - 1] + vegetablePrizes[vegetable - 1] + saucePrizes[sauce - 1];
+
+        client.SetBalance(shop, total);
+
+    Console.WriteLine($"That's the chad sandwich that you have composed there! It'il cost: {total}");
 
         return new Sandwich((Bun)bun, (Spread)spread, (Meat)meat, (Cheese)cheese, (Vegetable)vegetable, (Sauce)sauce);
     }
@@ -88,11 +149,24 @@ class SandwichShop
 
 class Client
 {
-    public decimal Balance { get; set; }
+    public decimal Balance { get; private set; }
 
     public Client()
     {
         Balance = 100.00m;
+    }
+
+    internal decimal SetBalance(object caller, decimal value)
+    {
+        if (caller is SandwichShop)
+        {
+            Balance = Balance - value;
+            return Balance;
+        }
+        else
+        {
+            throw new InvalidOperationException("You don't have permission.");
+        }
     }
 }
 record Sandwich(Bun bun, Spread spread, Meat meat, Cheese cheese, Vegetable vegetable, Sauce sauce);
